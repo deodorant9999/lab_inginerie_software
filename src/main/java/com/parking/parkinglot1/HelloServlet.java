f26c7d77a2155556124bd653247db9325b056196
deodorant9999@gmail.com <deodorant9999@gmail.com>
2024-11-04 11:10:20 +0200

    initial commit

--- src/main/java/com/parking/parkinglot1/HelloServlet.java
+++ src/main/java/com/parking/parkinglot1/HelloServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    private String message;//
 
     public void init() {
         message = "Hello World!";
